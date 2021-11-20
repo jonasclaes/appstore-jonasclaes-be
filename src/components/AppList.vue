b8aef4f2b3cfad431d43c13bea8bc3bc5720acf6
Jonas Claes <jonas@jonasclaes.be>
2021-11-20 20:46:18 +0100

    Updated index

--- src/components/AppList.vue
+++ src/components/AppList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
-    <div class="bg-gray-800 text-white rounded-lg shadow-lg p-4 col-span-1 md:col-span-2">
+    <div class="bg-white rounded-lg shadow-lg p-4 col-span-1 md:col-span-2">
       <h1 class="font-semibold text-xl">App list</h1>
       <p>A list of all apps!</p>
     </div>
@@ -8,10 +8,16 @@
     <div v-for="app in apps" v-bind:key="app.id">
       <router-link
           :to="`/app/${app.id}`">
-        <div class="bg-gray-800 text-white rounded-lg shadow-lg p-4">
-          <h2 class="font-semibold text-lg">{{ app.name }}</h2>
-          <h3 class="text-gray-400">Made by {{ app.author }}</h3>
-          <p class="truncate">Trainplannr is route planner for destinations in Belgium. It will also show the physical train carriages that make up the train. Train timetables and planning will be obtained through an external API, provided by the NMBS and iRail. Train compositions are retrieved from NMBS and iRail API.</p>
+        <div class="bg-white rounded-lg shadow-lg p-4 flex gap-3 content-start">
+          <img :src="`/assets/img/${app.id}/${app.icon}`" alt="Icon" class="h-full max-h-20 shadow-lg">
+          <div class="flex flex-col overflow-x-hidden content-start">
+            <h2 class="font-semibold text-lg">{{ app.name }}</h2>
+            <div>
+              <span class="text-gray-400 text-sm pr-2 mr-2 border-gray-300 border-r-2">{{ app.author }}</span>
+              <span class="text-gray-400 text-sm">{{ app.category }}</span>
+            </div>
+            <p class="truncate">{{ app.description }}</p>
+          </div>
         </div>
       </router-link>
     </div>
