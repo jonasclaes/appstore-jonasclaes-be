cd4fac5ef01aa600ac2b3d39fff209180b92a5b5
Jonas Claes <jonas@jonasclaes.be>
2021-11-21 10:25:55 +0100

    Updated about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
-    <div class="bg-gray-800 text-white rounded-lg shadow-lg p-4">
+    <div class="rounded-lg shadow-lg p-4">
       <h1 class="font-semibold text-xl">About</h1>
       <p>This site is made for the hosting of apps made by Jonas Claes.</p>
     </div>
-    <div class="bg-gray-800 text-white rounded-lg shadow-lg p-4">
+    <div class="rounded-lg shadow-lg p-4">
       <h1 class="font-semibold text-xl">Developer</h1>
       <p>This site has been made by Jonas Claes.</p>
       <p>E-mail: <a href="mailto:jonas@jonasclaes.be" class="text-blue-500">jonas@jonasclaes.be</a></p>
