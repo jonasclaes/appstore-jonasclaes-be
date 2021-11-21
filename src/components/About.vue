e5a0ce9df2e4ba3c97b214aab8553c2fc69400da
Jonas Claes <jonas@jonasclaes.be>
2021-11-21 10:31:53 +0100

    About page background of cards

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
-    <div class="rounded-lg shadow-lg p-4">
+    <div class="bg-white rounded-lg shadow-lg p-4">
       <h1 class="font-semibold text-xl">About</h1>
       <p>This site is made for the hosting of apps made by Jonas Claes.</p>
     </div>
-    <div class="rounded-lg shadow-lg p-4">
+    <div class="bg-white rounded-lg shadow-lg p-4">
       <h1 class="font-semibold text-xl">Developer</h1>
       <p>This site has been made by Jonas Claes.</p>
       <p>E-mail: <a href="mailto:jonas@jonasclaes.be" class="text-blue-500">jonas@jonasclaes.be</a></p>
